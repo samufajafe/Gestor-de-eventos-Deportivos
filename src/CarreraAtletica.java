38458ccd7cb4008efe585ec82b2f1f7fcd3db706
samufajafer <samufajafer@gmail.com>
2025-09-28 19:28:49 -0600

    Prueba v2

--- src/CarreraAtletica.java
+++ src/CarreraAtletica.java
@@ -2,19 +2,9 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class CarreraAtletica extends ActividadDeportiva {
-    int cantidad;
 
-    public CarreraAtletica(String nombre, LocalDate fecha, LocalTime hora, String descripcion, ActividadDeportiva.Estado estado, int cantidad) {
+    public CarreraAtletica(String nombre, LocalDate fecha, LocalTime hora, String descripcion, Estado estado) {
         super(nombre, fecha, hora, descripcion, estado);
-        this.cantidad = cantidad;
-    }
-
-    public int getCantidad() {
-        return cantidad;
-    }
-
-    public void setCantidad(int cantidad) {
-        this.cantidad = cantidad;
     }
 }
 
