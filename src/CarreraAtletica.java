cfa2c4793644e3658bdcac832389f06aff3455da
archGabo07 <gabox9823@gmail.com>
2025-09-28 19:27:02 -0600

    Prueba

--- src/CarreraAtletica.java
+++ src/CarreraAtletica.java
@@ -2,10 +2,19 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class CarreraAtletica extends ActividadDeportiva {
-    public CarreraAtletica(String nombre, LocalDate fecha, LocalTime hora, String descripcion, Estado estado) {
+    int cantidad;
+
+    public CarreraAtletica(String nombre, LocalDate fecha, LocalTime hora, String descripcion, ActividadDeportiva.Estado estado, int cantidad) {
         super(nombre, fecha, hora, descripcion, estado);
+        this.cantidad = cantidad;
     }
 
+    public int getCantidad() {
+        return cantidad;
+    }
 
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
 }
 
