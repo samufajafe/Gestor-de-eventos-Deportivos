ab59defcf5651285652dd36621542f356c5226df
samufajafer <samufajafer@gmail.com>
2025-09-28 19:25:18 -0600

    v6

--- src/CarreraAtletica.java
+++ src/CarreraAtletica.java
@@ -2,19 +2,10 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class CarreraAtletica extends ActividadDeportiva {
-    String lugar;
-
-    public CarreraAtletica(String nombre, LocalDate fecha, LocalTime hora, String descripcion, Estado estado, String lugar) {
+    public CarreraAtletica(String nombre, LocalDate fecha, LocalTime hora, String descripcion, Estado estado) {
         super(nombre, fecha, hora, descripcion, estado);
-        this.lugar = lugar;
     }
 
-    public String getLugar() {
-        return lugar;
-    }
 
-    public void setLugar(String lugar) {
-        this.lugar = lugar;
-    }
 }
 
